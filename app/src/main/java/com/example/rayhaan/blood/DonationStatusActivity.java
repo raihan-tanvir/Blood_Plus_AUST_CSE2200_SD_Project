5c333bb8c734de18ab882fa75cbd19b49ce060c8
Raihan Tanvir <raihantanvir.96@gmail.com>
2019-01-24 20:14:48 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rayhaan/blood/DonationStatusActivity.java
@@ -0,0 +1,13 @@
+package com.example.rayhaan.blood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonationStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_status);
+    }
+}
